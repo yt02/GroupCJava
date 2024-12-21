d6d09974166e61f44cc33c8cdbb992ff1af3d9ea
Peiiyong <165624659+Peiiyong@users.noreply.github.com>
2024-12-21 20:55:22 +0800

    update leave

---
+++ Leave/Leave.java
@@ -0,0 +1,16 @@
+package Leave;
+import employee.Employee;
+
+public class Leave {
+    public static void applyLeave(Employee employee,int leaveTaken){
+        int leaveBalance=employee.getLeaveBalance();
+        if(leaveTaken>leaveBalance){
+            double penalty=employee.getSalary()*0.10;
+            employee.setSalary(employee.getSalary()-penalty);
+            System.out.println("Employee\t\t:"+employee.getName());
+            System.out.println("Penalty\t\t\t:RM "+penalty);
+            System.out.println("Remaining Salary\t:RM "+employee.getSalary());
+        }
+        employee.setLeaveBalance(leaveBalance-leaveTaken);
+    }
+}
